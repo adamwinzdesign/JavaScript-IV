8e8d30c3a831de5e282b7d4bdc451263ac9ff708
adamwinzdesign <adamwinzdesign@gmail.com>
2019-09-19 20:17:49 -0500

    finshed MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -40,11 +40,12 @@ class Student extends Person {
   listsSubjects() {
     return `${this.name}'s favorite subjects are ${this.favSubjects}.`
   }
-  PRAssignment(student, subject) {
-    return `${this.name} submitted a pull request for ${this.subject}`
+  
+  PRAssignment(subject) {
+    return `${this.name} submitted a pull request for ${subject}.`
   }
   sprintChallenge(subject) {
-    return `${student.name} has begun a sprint challenge on ${subject}`
+    return `${this.name} has begun a sprint challenge on ${subject}.`
   }
 }
 
@@ -54,10 +55,91 @@ class ProjectManager extends Instructor {
     this.gradClassName = attr.gradClassName,
     this.favInstructor = attr.favInstructor
   }
-  standUp(name, channel) {
-    return `${name} announces to ${channel}@channel that the time for standup has arrived!`
+  standUp(channel) {
+    return `${this.name} announces to ${channel} @channel that the time for standup has arrived!`
   }
   debugsCode(student, subject) {
-    return `${name} debugs ${student.name}'s code on ${subject}.`
+    return `${this.name} debugs ${student.name}'s code on ${subject}.`
   }
 }
+
+const adam = new Student({
+  name: 'Adam',
+  age: 41,
+  location: 'Columbia, MO, USA',
+  previousBackground: 'Insurance',
+  className: 'WEB24FT',
+  favSubjects: ['CSS', 'JavaScript']
+});
+
+const spidey = new Student({
+  name: 'Peter',
+  age: 17,
+  location: 'New York, NY',
+  previousBackground: 'Biology Student',
+  className: 'Bio 434',
+  favSubjects: ['Biology', 'Criminal Justice', 'Sarcasm']
+});
+
+const tony = new Instructor({
+  name: 'Tony',
+  age: 42,
+  location: 'New York, NY',
+  specialty: 'Electrical Engineering',
+  favLanguage: 'billionaire',
+  catchPhrase: 'Jarvis!'
+});
+
+const bruce = new Instructor({
+  name: 'Bruce',
+  age: 40,
+  location: 'Planet Hulk',
+  specialty: 'Nuclear Physics',
+  favLanguage: 'English',
+  catchPhrase: 'Hulk Smash!'
+});
+
+const thanos = new ProjectManager({
+  name: 'Thanos',
+  age: 5435,
+  location: 'Titan',
+  specialty: 'Genocide',
+  favLanguage: 'Violence',
+  catchPhrase: 'Perfectly balanced...as all things should be.'
+});
+
+const galactus = new ProjectManager({
+  name: 'Galactus',
+  age: 345345,
+  location: 'The Cosmic Egg',
+  specialty: 'Consuming Planets',
+  favLanguage: 'Heralds',
+  catchPhrase: 'There does not exist a mortal who can stand before Galactus.'
+})
+
+console.log(adam.speak());
+console.log(adam.age, adam.location);
+console.log(adam.PRAssignment('JavaScript'));
+console.log(adam.sprintChallenge('React'));
+
+console.log(spidey.speak());
+console.log(spidey.age, spidey.location);
+console.log(spidey.listsSubjects());
+
+console.log(tony.speak());
+console.log(tony.favLanguage);
+console.log(tony.grade(adam, `Quantum Mechanics`));
+
+console.log(bruce.speak());
+console.log(bruce.grade(spidey, 'biology'));
+console.log(bruce.demo('nuclear safety'))
+
+console.log(galactus.speak());
+console.log(galactus.catchPhrase);
+console.log(galactus.demo('destruction'));
+console.log(galactus.specialty);
+console.log(galactus.debugsCode(spidey, 'witty retorts'))
+
+console.log(thanos.debugsCode(adam, 'reality'));
+console.log(thanos.catchPhrase);
+console.log(thanos.standUp('Web24'));
